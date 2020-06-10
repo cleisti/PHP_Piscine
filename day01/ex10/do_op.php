17ece0c77e1e8aa7ece5940122c80354f53e99c8
Camilla Leisti <camilla.leisti@gmail.com>
2020-06-10 16:24:22 +0300

    do_op works

--- day01/ex10/do_op.php
+++ day01/ex10/do_op.php
@@ -1,26 +1,19 @@
 #!/usr/bin/php
 <?php
-	function count_func($argv)
-	{
-		if (!is_numeric($argv[1]) || !is_numeric(argv[3]))
-			return (0);
-		$num1 = $argv[1];
-		$num2 = $argv[3];
-		$operators = "+-*/";
-		if (!strstr($operators, $argv[2]))
-			return (0);
-		if ($argv[2] == $operators[0])
-				$res = $num1 + $num2;
-		else if ($argv[2] == $operators[1])
-			$res = $num1 - $num2;
-		else if ($argv[2] == $operators[2])
-			$res = $num1 * $num2;
-		else
-			$res = $num1 / $num2;
-		print(res);
-	}
-	if ($argc != 4)
-		echo "Incorrect Parameters\n";
-	else if(!count_func($argv))
-		echo "Incorrect Parameters\n";
+if ($argc == 4)
+{
+	if (strstr($argv[2], '+'))
+		print($argv[1] + $argv[3]);
+	else if (strstr($argv[2], '-'))
+		print($argv[1] - $argv[3]);
+	else if (strstr($argv[2], '*'))
+		print($argv[1] * $argv[3]);
+	else if (strstr($argv[2], '/'))
+		print($argv[1] / $argv[3]);
+	else if (strstr($argv[2], '%'))
+		print($argv[1] % $argv[3]);
+	print "\n";
+}
+else
+	echo "Incorrect Parameters\n";
 ?>
