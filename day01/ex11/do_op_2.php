c7068aa46375dcacf07a45d34f78e69fa7361934
Camilla Leisti <camilla@91-155-210-96.elisa-laajakaista.fi>
2020-06-11 11:21:20 +0300

    do_op_2 works

--- day01/ex11/do_op_2.php
+++ day01/ex11/do_op_2.php
@@ -2,10 +2,30 @@
 <?php
 	if ($argc == 2)
 	{
-		if (preg_match('\p{L})')
-		preg_match_all('!\d+!', $argv[1], $numbers);
-		print_r($numbers);
+		if (!preg_match('/^[%*+\-\/\ 0-9]/', $trimmed))
+			exit ("Syntax Error\n");
+		
+		$trimmed = trim(preg_replace('/\s+/', '', $argv[1]));
+		$operator = strpbrk($trimmed, "+-*/%");
+		$numbers = preg_split('/[%*+\/\-\\\\]/', $trimmed);
+
+		if (count($numbers) != 2)
+			exit ("Syntax Error\n");
+		
+		if ($operator[0] == '+')
+			print($numbers[0] + $numbers[1]);
+		else if ($operator[0] == '-')
+			print($numbers[0] - $numbers[1]);
+		else if ($operator[0] == '*')
+			print($numbers[0] * $numbers[1]);
+		else if ($operator[0] == '/')
+			print($numbers[0] / $numbers[1]);
+		else if ($operator[0] == '%')
+			print($numbers[0] % $numbers[1]);
+		echo "\n";
 	}
 	else
 		echo "Incorrect Parameters\n";
 ?>
+
+// works fine but could be structured better
