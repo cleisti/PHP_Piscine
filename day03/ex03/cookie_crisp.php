d26f32653c043b7cafbd1dfbe230875b571a1ce1
Camilla Leisti <camilla.leisti@gmail.com>
2020-06-16 11:49:52 +0300

    cookie crisp fized

--- day03/ex03/cookie_crisp.php
+++ day03/ex03/cookie_crisp.php
@@ -1,6 +1,6 @@
 <?php
 if ($_GET['action'] === 'set' && $_GET['name'] && $_GET['value'])
-    setcookie($_GET['name'], $_GET['name'], time() + 3600);
+    setcookie($_GET['name'], $_GET['value'], time() + 3600);
 else if ($_GET['action'] === 'get' && $_GET['name'])
     echo $_COOKIE[$_GET['name']]."\n";
 else if ($_GET['action'] === 'del' && $_GET['name'])
