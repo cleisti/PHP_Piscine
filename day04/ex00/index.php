7e2def45694bfcbca28daed241528f82c479f3f6
Camilla Leisti <camilla.leisti@gmail.com>
2020-06-16 17:28:22 +0300

    session

--- day04/ex00/index.php
+++ day04/ex00/index.php
@@ -1,3 +1,23 @@
 <?php
+header('content-type: text/plain');
 session_start();
+
+if ($_GET['submit'] === 'OK')
+{
+	if ($_GET['login'] && $_GET['passwd'])
+	{
+		$_SESSION['user'] = $_GET['login'];
+		$_SESSION['passwd'] = $_GET['passwd'];
+	}
+}
 ?>
+
+<html>
+<body>
+<form action="" method="get">
+Username: <input type="text" name="login" value="<?php = $_SESSION['user'] ?>"/><br />
+Password: <input type="text" name="passwd" value="<?php = $_SESSION['passwd'] ?>"/><br />
+	<input type="submit" name="submit" value="OK"/>"
+</form>
+</body>
+</html>
