8d41b87a4e8005fc5476ce709a5e0f46b2ffa8a2
Camilla Leisti <camilla.leisti@gmail.com>
2020-06-18 09:31:37 +0300

    index.php works

--- day04/ex00/index.php
+++ day04/ex00/index.php
@@ -2,22 +2,21 @@
 header('content-type: text/plain');
 session_start();
 
-if ($_GET['submit'] === 'OK')
+if (isset($_GET['submit']) && $_GET['submit'] === 'OK')
 {
-	if ($_GET['login'] && $_GET['passwd'])
+	if (isset($_GET['login']) && isset($_GET['passwd']))
 	{
-		$_SESSION['user'] = $_GET['login'];
+		$_SESSION['login'] = $_GET['login'];
 		$_SESSION['passwd'] = $_GET['passwd'];
 	}
 }
 ?>
 
-<html>
-<body>
+<html><body>
 <form action="" method="get">
-Username: <input type="text" name="login" value="<?php = $_SESSION['user'] ?>"/><br />
-Password: <input type="text" name="passwd" value="<?php = $_SESSION['passwd'] ?>"/><br />
-	<input type="submit" name="submit" value="OK"/>"
+	Username: <input type="text" name="login" value="<?= $_SESSION['login']; ?>" />
+	<br />
+	Password: <input type="text" name="passwd" value="<?= $_SESSION['passwd']; ?>" />
+	<input type="submit" name="submit" value="OK" />"
 </form>
-</body>
-</html>
+</body></html>
