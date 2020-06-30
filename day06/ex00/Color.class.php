6bf2e6cdf40cd7b82ada10e1c2f85acae0685381
Camilla Leisti <camilla.leisti@gmail.com>
2020-06-30 11:41:07 +0300

    ex00

--- day06/ex00/Color.class.php
+++ day06/ex00/Color.class.php
@@ -3,18 +3,18 @@ class Color {
     public $red;
     public $green;
     public $blue;
-    static $verbose = FALSE;
+    public static $verbose = FALSE;
 
     public function __construct($array)
     {
-        if ($array['rgb'])
+        if (isset($array['rgb']))
         {
             $rgb = intval($array['rgb']);
             $this->red = ($rgb >> 16) & 0xFF;
             $this->green = ($rgb >> 8) & 0xFF;
             $this->blue = ($rgb & 0xFF);
         }
-        else if ($array['red'] && $array['green'] && $array['blue'])
+        else if (isset($array['red']) && isset($array['green']) && isset($array['blue']))
         {
             $this->red = intval($array['red']);
             $this->green = intval($array['green']);
@@ -26,7 +26,7 @@ class Color {
 
     public function __destruct()
     {
-        if (self::verbose)
+        if (self::$verbose)
             print($this . ' destructed.' . PHP_EOL);
     }
 
@@ -43,17 +43,29 @@ class Color {
 	
 	public function add( Color $rhs)
 	{
-
+		$new = new Color ( array(
+			'red' => $this->red + $rhs->red,
+			'green' => $this->green + $rhs->green,
+			'blue' => $this->blue + $rhs->blue, ));
+		return ($new);
 	}
 
 	public function sub( Color $rhs )
 	{
-
+		$new = new Color ( array(
+			'red' => $this->red - $rhs->red,
+			'green' => $this->green - $rhs->green,
+			'blue' => $this->blue - $rhs->blue	));
+		return ($new);
 	}
 
 	public function mult( $f )
 	{
-
+		$new = new Color ( array(
+			'red' => $this->red * $f,
+			'green' => $this->green * $f,
+			'blue' => $this->blue * $f ));
+		return ($blue);
 	}
 }
 ?>
